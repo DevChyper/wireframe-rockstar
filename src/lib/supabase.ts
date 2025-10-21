824ef48cb47aace19d84f043ef6ee247b945e91e
DevChyper <118332769+DevChyper@users.noreply.github.com>
2025-10-21 18:03:16 +0700

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://uyjfjwioljczsvmedxgt.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV5amZqd2lvbGpjenN2bWVkeGd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEwMDQxNjEsImV4cCI6MjA3NjU4MDE2MX0.xBpCjdozQDikETZ7RaK5-efQr0fE3SnAV2Cxn0aL6Hg";
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing Supabase environment variables');
